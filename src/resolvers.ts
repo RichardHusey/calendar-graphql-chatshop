0eeabd73b020806a86c2f1c271432e1b404cf296
DESKTOP-OR28K8I\richard <chrisgogo68@gmail.com>
2023-02-15 10:47:29 -0500

    feat design schema, imlemented query with proof validation

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -2,43 +2,86 @@ import axios from "axios";
 const BASE_PATH = "http://calapi.inadiutorium.cz/api/";
 
 const calendarRequest = async (version: String, language: String, calType: String, date? : String | Number) => {
-    //version validation
-    const versionExpression = /^v\d+$/;
-    const isValidVersion = versionExpression.test(String(version).toLowerCase());
-  
-    if (!isValidVersion) {
-      console.log(version, "process is suspended due to .//");
-      throw new Error("Version not in proper format");
-    }
-    //language validation
-    if (language !== ("en" || "fr" || "it" || "la" || "cs"))
-      throw new Error("This Language is not supported by App");
-    //calType validation
-    if (calType !== ("default" || "general-la" || "general-en" || "czech"))
-      throw new Error("This Calendar Type in not supported by App");
-  
-    const request_url = `${BASE_PATH}${version}/${language}/calendars/${calType}/${date}`;
-    const res = await axios.get(request_url);
-    return res.data;
-  };
-  
-  interface IQueryType { 
-      version: String,
-      language: String,
-      calType: String
+  //version validation
+  const versionExpression = /^v\d+$/;
+  const isValidVersion = versionExpression.test(String(version).toLowerCase());
+
+  if (!isValidVersion) {
+    throw new Error("Version not in proper format");
   }
-  
+  //language validation
+  if (language !== ("en" || "fr" || "it" || "la" || "cs"))
+    throw new Error("This Language is not supported by App");
+  //calType validation
+  if (calType !== ("default" || "general-la" || "general-en" || "czech"))
+    throw new Error("This Calendar Type in not supported by App");
+
+  const request_url = `${BASE_PATH}${version}/${language}/calendars/${calType}/${date}`;
+  const res = await axios.get(request_url);
+  return res.data;
+};
+
+interface IQueryType { 
+    version: String,
+    language: String,
+    calType: String
+}
+
+interface IYearQueryType extends IQueryType {
+    year: number
+}
+
+interface IMonthQueryType extends IYearQueryType {
+    month: number
+}
+
+interface IDayQueryType extends IQueryType {
+    day: String
+}
+
 const Query = {
-    system: async (root:unknown, args: IQueryType) => {
-      const { version, language, calType } = args;
-      try {
-        const res = await calendarRequest(version, language, calType);
-        return res.data;
-      } catch (err) {
-        return err;
-      }
+  system: async (root:unknown, args: IQueryType) => {
+    const { version, language, calType } = args;
+    try {
+      const res = await calendarRequest(version, language, calType);
+      return res.data;
+    } catch (err) {
+      return err;
     }
-  };
-  
+  },
+  year: async (root:unknown, args: IYearQueryType) => {
+    const { version, language, calType, year } = args;
+    try {
+      const res = await calendarRequest(version, language, calType, year);
+      return res;
+    } catch (err) {
+      return err;
+    }
+  },
+  month: async (root:unknown, args: IMonthQueryType) => {
+    const { version, language, calType, year, month } = args;
+    //month validation
+    try {
+      const res = await calendarRequest(
+        version,
+        language,
+        calType,
+        `${year}/${month}`
+      );
+      return res;
+    } catch (err) {
+      return err;
+    }
+  },
+  day: async (root:unknown, args: IDayQueryType) => {
+    const { version, language, calType, day } = args;
+    try {
+      const res = await calendarRequest(version, language, calType, day);
+      return res;
+    } catch (err) {
+      return err;
+    }
+  },
+};
 
 export default { Query };
